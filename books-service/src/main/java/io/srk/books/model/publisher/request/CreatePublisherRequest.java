da4ec44d03f97495cf95a7964af96bd576d5807d
Oleksandr Soroka <sasha20999@outlook.com>
2024-01-18 18:28:33 +0200

    fix publisher request constraints

--- books-service/src/main/java/io/srk/books/model/publisher/request/CreatePublisherRequest.java
+++ books-service/src/main/java/io/srk/books/model/publisher/request/CreatePublisherRequest.java
@@ -1,9 +1,11 @@
 package io.srk.books.model.publisher.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CreatePublisherRequest {
 
+    @NotBlank(message = "Name cannot be blank")
     private String name;
 }
--- books-service/src/main/java/io/srk/books/model/publisher/request/UpdatePublisherRequest.java
+++ books-service/src/main/java/io/srk/books/model/publisher/request/UpdatePublisherRequest.java
@@ -1,9 +1,11 @@
 package io.srk.books.model.publisher.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class UpdatePublisherRequest {
 
+    @NotBlank(message = "Name cannot be blank")
     private String update;
 }
