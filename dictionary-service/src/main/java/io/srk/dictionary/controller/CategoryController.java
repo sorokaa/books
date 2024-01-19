25d519e7cae3c6e84d1e791d534c76901063c9a1
Oleksandr Soroka <sasha20999@outlook.com>
2024-01-20 00:11:48 +0200

    add API logs

--- dictionary-service/src/main/java/io/srk/dictionary/controller/CategoryController.java
+++ dictionary-service/src/main/java/io/srk/dictionary/controller/CategoryController.java
@@ -5,6 +5,7 @@ import io.srk.dictionary.service.category.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/dictionaries/categories")
 @RequiredArgsConstructor
@@ -23,18 +25,21 @@ public class CategoryController {
     @Operation(summary = "Get all categories")
     @GetMapping
     public List<CategoryDto> getAll() {
+        log.debug("API request to get all categories");
         return categoryService.getAll();
     }
 
     @Operation(summary = "Get category by id")
     @GetMapping("/{id}")
     public CategoryDto getById(@PathVariable Long id) {
+        log.debug("API request to get category by id {}", id);
         return categoryService.getById(id);
     }
 
     @Operation(summary = "Get category existence by id")
     @GetMapping("/{id}/exists")
     public boolean existsById(@PathVariable Long id) {
+        log.debug("API request to check category existence by id {}", id);
         return categoryService.existsById(id);
     }
 }
--- dictionary-service/src/main/java/io/srk/dictionary/controller/LanguageController.java
+++ dictionary-service/src/main/java/io/srk/dictionary/controller/LanguageController.java
@@ -5,6 +5,7 @@ import io.srk.dictionary.service.language.LanguageService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/dictionaries/languages")
 @RequiredArgsConstructor
@@ -23,18 +25,21 @@ public class LanguageController {
     @Operation(summary = "Get all languages")
     @GetMapping
     public List<LanguageDto> getLanguages() {
+        log.debug("API request to get all languages");
         return languageService.getAll();
     }
 
     @Operation(summary = "Get language by id")
     @GetMapping("/{id}")
     public LanguageDto getById(@PathVariable Long id) {
+        log.debug("API request to get language by id {}", id);
         return languageService.getById(id);
     }
 
     @Operation(summary = "Get language existence by id")
     @GetMapping("/{id}/exists")
     public boolean existsById(@PathVariable Long id) {
+        log.debug("API request to check language existence by id {}", id);
         return languageService.existsById(id);
     }
 }
